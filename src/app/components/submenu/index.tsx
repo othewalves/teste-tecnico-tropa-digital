975561b4f862d646b47101a5c996509f12f03687
Matheus Alves <matheusfrancisco0330@gmail.com>
2025-06-10 13:45:41 -0300

    feat: add icons

--- src/app/components/submenu/index.tsx
+++ src/app/components/submenu/index.tsx
@@ -1,11 +1,21 @@
+import Image from 'next/image';
 import style from './styles.module.scss';
 
 const Submenu = () => {
     return (
         <div className={style.container}>
-            <button className={style.button}>Visualizar</button>
-            <button className={style.button}>Editar</button>
-            <button className={style.button}>Remover</button>
+            <button className={style.button}>
+                <Image width={16} height={16} src={'/Eye.svg'} alt='Visualizar' />
+                Visualizar
+            </button>
+            <button className={style.button}>
+                <Image width={16} height={16} src={'/Pencil.svg'} alt='Editar' />
+                Editar
+            </button>
+            <button className={`${style.button}${style.buttonTrash}`}>
+                <Image width={16} height={16} src={'/Trash.svg'} alt='Remover' />
+                Remover
+            </button>
         </div>
     );
 }
